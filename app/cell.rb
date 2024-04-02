6ebde0af5e94e9899b212ea8a600463e16d93a43
Piotr Usewicz <piotr@layer22.com>
2024-04-02 08:22:54 +0200

    Fix to_s

--- app/cell.rb
+++ app/cell.rb
@@ -29,7 +29,7 @@ class Cell
   end
 
   def to_s
-    "Cell #{@x}x#{@y} collapsed: #{@collapsed}, entropy: #{@entropy}, name: #{@name}"
+    "Cell #{@x}x#{@y} collapsed: #{@collapsed}, entropy: #{@entropy}, tile: #{name}"
   end
 
   def options=(options)
